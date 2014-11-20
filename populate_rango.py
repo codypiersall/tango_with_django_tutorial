b3631955db0903586533a3aab3e06f0027c608ea
Cody Piersall <cwp@questconsult.com>
2014-11-20 13:31:44 -0600

    Fix typo and add script for removing all objects.

--- populate_rango.py
+++ populate_rango.py
@@ -9,7 +9,7 @@ from rango.models import Category, Page
 
 
 def populate():
-    python_cat = add_cat('Python')
+    python_cat = add_cat('Python', views=128, likes=64)
 
     add_page(cat=python_cat,
              title='Official Python Tutorial',
@@ -23,7 +23,7 @@ def populate():
              title="Learn Python in 10 Minutes",
              url="http://www.korokithakis.net/tutorials/python/")
 
-    django_cat = add_cat('Django')
+    django_cat = add_cat('Django', views=64, likes=32)
 
     add_page(cat=django_cat,
              title="Official Django Tutorial",
@@ -33,7 +33,7 @@ def populate():
              title="How to =Tango with Django",
              url='http://www.tangowithdjango.com/')
 
-    frame_cat = add_cat('Ohter Frameworks')
+    frame_cat = add_cat('Other Frameworks', views=32, likes=16)
 
     add_page(frame_cat,
              title='Bottle',
@@ -48,8 +48,8 @@ def populate():
             print("- {} - {}'".format(str(c), str(p)))
 
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name, views=0, likes=0):
+    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
     return c
 
 
@@ -60,4 +60,10 @@ def add_page(cat, title, url, views=0):
 
 if __name__ == '__main__':
     print('Starting Rango population script...')
-    populate()
+    import sys
+    if len(sys.argv)> 1 and sys.argv[1] == 'delete':
+        print('deleting all objects')
+        Category.objects.all().delete()
+        Page.objects.all().delete()
+    else:
+        populate()

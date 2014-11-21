f1ca9d6e696595ca69be980158e846b608c21eef
Cody Piersall <cody.piersall@gmail.com>
2014-11-20 21:23:58 -0600

    Add view for index, and add slug for Category.

--- rango/views.py
+++ rango/views.py
@@ -1,17 +1,17 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-
+from rango.models import Category
 
 # Create your views here.
 def index(request):
+    # Query the database for a list of all categories currently stored.
+    # Order the categories by number of likes in descending order.
+    # Retrieve the top 5 only - or all if less than 5.
+    # Place the list in our context_dict dictionary which will be passed
+    # to the template engine.
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
 
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': 'I am bold font from the context'}
-
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
+    # Render the response and sent it back!
     return render(request, 'rango/index.html', context_dict)
 
 

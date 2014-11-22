c748305df484ab693dcad32213425c668c269603
Cody Piersall <cody.piersall@gmail.com>
2014-11-22 09:04:11 -0600

    Add most viewed pages to index.

--- rango/views.py
+++ rango/views.py
@@ -9,7 +9,9 @@ def index(request):
     # Place the list in our context_dict dictionary which will be passed
     # to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    pages = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list,
+                    'pages': pages}
 
     # Render the response and sent it back!
     return render(request, 'rango/index.html', context_dict)
